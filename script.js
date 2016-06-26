573156bf7a9f2b55d06d0e30c5d7943446f583b4
Jennifer Cen <cenjennifer@gmail.com>
2016-06-26 00:47:29 -0400

    incorporated lodash and browserify

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-var checkPriceStatus = function() {
-    chrome.storage.local.get({ pageInfo: [] }, function(response) {
-        response.pageInfo.forEach(function(singlePage) {
-            var pageUrl = Object.keys(singlePage)[0];
+let checkPriceStatus = function() {
+    window.chrome.storage.local.get({ pageInfo: [] }, function(response) {
+        console.log(response);
+        if (response.pageInfo.length > 1) {
+            _.forEach(response.pageInfo, function(singlePage) {
+                let url = _.keys(singlePage)[0];
+                let pageUrlSelection = singlePage[url].selection[0];
+              
 
-            $.ajax({
-                url: pageUrl,
-                error: function() {
-                    console.log("There is an error");
-                },
-                success: function(data) {
-                    var status = data.includes(singlePage[pageUrl][0].selection[0].price[0]);
-                    var name = singlePage[pageUrl][0].selection[0].words[0];
-                    console.log(status, name);
-                    if (!status) {
-                        var itemSelector = $('.itemName:contains("' + name + '")');
-                  
-                        itemSelector.parents('.eachWatch').css({ "background-color": "#b5e1f7", "color": "#0b2035" });
-
-                        chrome.runtime.sendMessage({ changed: true });
-
+                $.ajax({
+                    url: url,
+                    error: function() {
+                        console.log("There is an error");
+                    },
+                    success: function(data) {
+                        let status = data.includes(pageUrlSelection.price[0]);
+                        let name = pageUrlSelection.words[0];
+                        if (!status) {
+                            let itemSelector = $('.itemName:contains("' + name + '")');
+                            itemSelector.parents('.eachWatch').css({ "background-color": "#b5e1f7", "color": "#0b2035" });
+                            chrome.runtime.sendMessage({ changed: true });
+                        }
                     }
-                }
+
+                });
 
             });
+        }
 
-        });
     });
 };
 
-// window.setInterval(function() { checkPriceStatus(); }, 5000);
+window.setInterval(function() { checkPriceStatus(); }, 100000);
